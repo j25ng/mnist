ada9adf2435b5d3be97bbde114a269ae8698eaa0
j25ng <jsan2861@gmail.com>
2024-09-20 17:36:19 +0900

    sub

---
+++ src/mnist/worker.py
@@ -0,0 +1,16 @@
+from rightnow.time import now
+
+def run():
+  """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
+  
+  # STEP 1
+  # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 가져오기 
+
+  # STEP 2
+  # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
+  # 동시에 prediction_model, prediction_time 도 업데이트
+
+  # STEP 3
+  # LINE 으로 처리 결과 전송
+
+  print(now())
