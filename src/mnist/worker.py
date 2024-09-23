d740bef3157dfcf7c69605425b2cd1821cc5d7d8
j25ng <jsan2861@gmail.com>
2024-09-23 16:36:08 +0900

    commit

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -1,16 +1,55 @@
 from rightnow.time import now
+from mnist.db import select, dml
+import random
+
+def get_job_img_task():
+    sql = """
+    SELECT
+    num, file_name, file_path
+    FROM image_processing
+    WHERE prediction_result IS NULL
+    ORDER BY num
+    LIMIT 1
+    """
+    r = select(sql, 1)
+
+    if len(r) > 0:
+        return r[0]
+    else:
+        return None
+
+def prediction(file_path, num):
+    sql = """UPDATE image_processing
+    SET prediction_result=%s,
+        prediction_model='n16',
+        prediction_time=%s
+    WHERE num=%s
+    """
+    presult = random.radint(0, 9)
+    dml(sql, random.presult, now(), num)
+    
+    return presult
 
 def run():
-  """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
-  
-  # STEP 1
-  # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 가져오기 
+    """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
+    # STEP 1
+    # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 가져오기 
+    job = get_job_img_task()
 
-  # STEP 2
-  # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
-  # 동시에 prediction_model, prediction_time 도 업데이트
+    if job is None:
+        print(f"{now()} - job is None")
+        return
 
-  # STEP 3
-  # LINE 으로 처리 결과 전송
+    num = job['num']
+    file_name = job['file_name']
+    file_path = job['file_path']
 
-  print(now())
+    # STEP 2
+    # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
+    # 동시에 prediction_model, prediction_time 도 업데이트
+    presult = prediction(file_path, num)
+
+    # STEP 3
+    # LINE 으로 처리 결과 전송
+
+    print(now())
