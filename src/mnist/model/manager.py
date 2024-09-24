baa57a604e94f424edd947e72df50a72c8c0079a
j25ng <jsan2861@gmail.com>
2024-09-24 17:11:12 +0900

    fix file name

--- src/mnist/model/manager.py
+++ src/mnist/model/manager.py
@@ -4,5 +4,5 @@ def get_model_path():
     f = __file__
     path = os.path.abspath(f)
     dir_name = os.path.dirname(path)
-    pkl_path = os.path.join(dir_name, "mnist240924.keras")
-    return pkl_path
+    model_path = os.path.join(dir_name, "mnist240924.keras")
+    return model_path
